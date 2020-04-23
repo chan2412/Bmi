f17bafe8c17a2eeca1c451300dffcc86ac1dceb0
CHANDHRU R <44549194+chan2412@users.noreply.github.com>
2020-04-23 09:57:19 +0530

    Updates

---
+++ precache-manifest.85f14becb4b774328cd1ed1ef3b51f86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05e443786212ce1f162fa3f40a024a3a",
+    "url": "/Bmi/index.html"
+  },
+  {
+    "revision": "c0b0016f8ce9b6a9745c",
+    "url": "/Bmi/static/css/main.4b9f825b.chunk.css"
+  },
+  {
+    "revision": "6d72854ec142b88549be",
+    "url": "/Bmi/static/js/2.7f5ca8e2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Bmi/static/js/2.7f5ca8e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0b0016f8ce9b6a9745c",
+    "url": "/Bmi/static/js/main.19013985.chunk.js"
+  },
+  {
+    "revision": "282d4cd07dec003df444",
+    "url": "/Bmi/static/js/runtime-main.ee1d1cdc.js"
+  }
+]);
